87d2a9c05e0bdac8af665f567da738b8c5338e7d
Shanelle Webster <webstershanelle@gmail.com>
2019-01-01 13:52:16 -0600

    form styling done

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,17 +1,29 @@
 <template>
-  <v-form ref="form" v-model="valid" lazy-validation>
-    <v-text-field v-model="name" :rules="nameRules" :counter="10" label="Name" required></v-text-field>
-    <v-text-field v-model="price" :rules="priceRules" label="Price" required></v-text-field>
-    <v-text-field v-model="imageurl" :rules="imageRules" label="Image URL" required></v-text-field>
-    <v-checkbox
-      v-model="checkbox"
-      :rules="[v => !!v || 'You must agree to continue!']"
-      label="Add to Inventory"
-      required
-    ></v-checkbox>
-    <v-btn :disabled="!valid" @click="submit">Submit</v-btn>
-    <v-btn @click="clear">Clear</v-btn>
-  </v-form>
+  <div class="form-container">
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field v-model="name" :rules="nameRules" :counter="10" label="Name" required></v-text-field>
+      <v-text-field v-model="price" :rules="priceRules" label="Price" required></v-text-field>
+      <v-text-field v-model="imageurl" :rules="imageRules" label="Image URL" required></v-text-field>
+      <div class="container">
+        <img class="image-upload" v-if="imageurl.length > 10" v-bind:src="imageurl">
+        <img
+          class="image-upload"
+          v-else
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKZ3Fo6lvvr9t9hi1hs_dG6MnQRmaNh5qO-jdPeWAj8eiGW6mO"
+        >
+      </div>
+      <v-checkbox
+        v-model="checkbox"
+        :rules="[v => !!v || 'You must agree to continue!']"
+        label="Add to Inventory"
+        required
+      ></v-checkbox>
+      <div class="container">
+        <v-btn :disabled="!valid" @click="submit">Submit</v-btn>
+        <v-btn @click="clear">Clear</v-btn>
+      </div>
+    </v-form>
+  </div>
 </template>
 
 <script>
@@ -57,4 +69,19 @@ export default {
 </script>
 
 <style>
+.form-container {
+  width: 40vw;
+  box-shadow: 1px 1px 20px 3px rgba(143, 143, 143, 0.281);
+  padding: 2vw;
+}
+.container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.image-upload {
+  width: auto;
+  height: 20vh;
+  margin: 3vw;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,7 +16,7 @@ export default {
 <style>
 .header {
   height: 5vh;
-  box-shadow: 10px 3px 10px 5px rgba(143, 143, 143, 0.2);
+  box-shadow: 1px 1px 20px 3px rgba(143, 143, 143, 0.281);
   margin-bottom: 5vh;
 }
 </style>
